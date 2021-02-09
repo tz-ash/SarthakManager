cea568d618c3637113d208788f13e8a035b3a701
sᴡᴏɴɪᴛ <72299804+swatv3nub@users.noreply.github.com>
2021-02-09 15:58:24 +0530

    Ok

--- Hackfreaks/modules/backups.py
+++ Hackfreaks/modules/backups.py
@@ -334,7 +334,7 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("IGRISROBOT{}.backup".format(chat_id), "rb"),
+        document=open("{}_export.txt".format(chat_id), "rb"),
         caption="*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `Hackfreaks-Backup` was specially made for notes."
         .format(chat.title, chat_id, tgl),
         timeout=360,
